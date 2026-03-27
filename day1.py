8d748ba39c75402cee77d9570efc05a1f54b743b
Yan <yanfist@vip.qq.com>
2026-03-27 15:53:26 +0800

    第一天学习结束

--- day1.py
+++ day1.py
@@ -1,3 +1,11 @@
 name = input("请输出你的名字：")
 
 print("Hello VS Code", "\n你好", "\n" + name)
+
+a = 10
+b = 3
+
+print(a + b) # 加法
+print(a - b) # 减法
+print(a * b) # 乘法
+print(a / b) # 除法
---
+++ day1_1.py
@@ -0,0 +1,19 @@
+# 1.欢迎
+print("==== Python学习第1天 打卡 ====")
+
+# 2.获取用户输入
+name = input("请输入你的名字：")
+age = input("请输入你的年龄：")
+
+# 3.输出信息
+print("你的名字是：", name)
+print("你的年龄是：", age)
+
+# 4. 简单计算
+num1 = 10
+num2 = 20
+sum_result = num1 + num2
+print("10 +20 的计算结果是：", sum_result)
+
+# 5.结束提示
+print("==== 今日联系完成 ====")
