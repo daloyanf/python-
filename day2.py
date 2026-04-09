9fc7e420255923d6c24802e000b91b1d70b4eb8d
Yan <yanfist@vip.qq.com>
2026-04-09 12:58:46 +0800

    无

---
+++ day2.py
@@ -0,0 +1,13 @@
+name = "荔枝python"
+birth_year = 1994
+height = 1.78
+
+print(type(name))
+print(type(birth_year))
+print(type(height))
+
+num1 = "123456789"
+
+num2 = num1[-9:-5]
+
+print(num2)
---
+++ day2_1.py
@@ -0,0 +1,5 @@
+birth_year = int(input("请输入你的出生年份："))
+print(type(birth_year))
+
+age = 2026 - birth_year
+print("你的年龄是：" + str(age))
